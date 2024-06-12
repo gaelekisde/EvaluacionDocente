d168491e06af03b7877302c3bf81750d60b4ea10
gaelekisde <gael1411hernandez@gmail.com>
2024-06-12 15:58:32 -0600

    Inicial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+const port = 5000
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
+
