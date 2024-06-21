53b47c45dc3bebc47a95cad3f97f8a4e86b8bbcf
aredj1'ui <abisaicantu5@gmail.com>
2024-06-21 17:44:37 -0600

    cambio

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Register} from './pages/register/register';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+<Register />
+  </>
 );
+//<React.StrictMode>
+    
+//</React.StrictMode>
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- client/src/pages/register/register.jsx
+++ client/src/pages/register/register.jsx
@@ -1,9 +1,62 @@
-import "./register.scss"
+import "/Users/abisa/Desktop/EvaluacionDocente/client/src/pages/register/login.scss"
 
-const register = () => {
+export const Register = () => {
   return (
-    <div className="register">register</div>
-  )
-}
-
-export default register
+    <form onSubmit={() => alert("Enviado")}>
+      <p>
+        Nombre de usuario:
+        <input type="text" maxLength={20} placeholder="AredJ" />
+      </p>
+      <p>
+        Numero de control:
+        <input type="text" maxLength={9} placeholder="231G0168" />
+      </p>
+      <p>
+        Contraseña:
+        <input type="password" maxLength={200} placeholder="password" />
+      </p>
+      <p>
+        Semestre:
+        <select name="Semestre">
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+          <option value={6}>6</option>
+          <option value={7}>7</option>
+          <option value={8}>8</option>
+          <option value={9}>9</option>
+          <option value={10}>+9</option>
+        </select>
+      </p>
+      <p>
+        Carrera:
+        <select name="carrera">
+          <option value={"Sistemas computacionales"}>
+            Ing. en Sistemas computacionales
+          </option>
+          <option value={"Industrial"}>Ing. Industrial</option>
+          <option value={"Mecatronica"}>Ing. Mecatronica</option>
+          <option value={"Electromecanica"}>Ing. Electromecanica</option>
+          <option value={"Administracion"}>Ing. Administracion</option>
+          <option value={"Petrolera"}>Ing. Petrolera</option>
+          <option value={"Animacion digital y efectos visuales"}>
+            Ing. Animacion digital y efectos visuales
+          </option>
+          <option value={"Quimica"}>Ing. Quimica</option>
+          <option value={"Sabatina"}>Modalidad Sabatina</option>
+        </select>
+      </p>
+      <p>
+        Nombre(s):
+        <input type="text" maxLength={15} placeholder="Jared Abisai" />
+      </p>
+      <p>
+        Apellidos:
+        <input type="text" maxLength={20} placeholder="Cantu Rodriguez" />
+      </p>
+      <input type="submit" value={"enviar"} />
+    </form>
+  );
+};
