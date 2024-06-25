adc476f852b745c2757a288aeab318b51b457820
gaelekisde <gael1411hernandez@gmail.com>
2024-06-24 20:27:12 -0600

    cambios

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -1,14 +1,29 @@
 import {db} from "../connect.js"
 
-export const getPostsistemas = (req, res) => {
-    const opinion_id = req.params.opinion_id;
-
-    const q = `select * from opiniones where carrera = 'sistemas' and opinion_id = ?`
-    db.query(q, [opinion_id], (err, result) => {
+export const getPosts = (req, res) => {
+    const carrera = req.params.carrera;
+    const query = 'SELECT * FROM opiniones WHERE carrera = ?';
+    db.query(query, [carrera], (err, results) => {
         if (err) {
-            return res.status(500).json({ error: 'Database query failed' });
+            console.error('Error fetching posts:', err);
+            return res.status(500).json({ error: 'Error fetching posts' });
         }
-        // Send the result as the response
-        return res.status(200).json(result);
+        if (results.length === 0) {
+            return res.status(404).json({ error: 'No posts found for this carrera' });
+        }
+        return res.status(200).json(results);
     });
+};
+
+export const getPost = (req, res) => {
+    const opinion_id = req.params.opinion_id;
+    const carrera = req.params.carrera;
+
+    const query = `select * from opiniones where carrera = '${carrera}' and opinion_id = ?`
+    console.log(query)
+    
+    db.query(query, [opinion_id], (err, result) => {
+        if (err) throw err
+        return res.status(200).json(result)
+    })   
 }
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,14 +1,10 @@
 import express from "express"
-import { getPostsistemas }from "../controllers/posts.js"
+import { getPosts, getPost }from "../controllers/posts.js"
 
 const router = express.Router();
 
-router.get('/sistemas/:opinion_id', getPostsistemas)
-// router.get('/animacion/:opinion_id', getPostanimacion)
-// router.get('/electromecanica/:opinion_id', getPostelectromecanica)
-// router.get('/industrial/:opinion_id', getPostindustrial)
-// router.get('/mecatronica/:opinion_id', getPostmecatronica)
-// router.get('/petrolera/:opinion_id', getPostpetrolera)
-// router.get('/quimica/:opinion_id', getPostquimica)
+// router.get('/sistemas/posts/', getPostsdesistemas)
+router.get('/:carrera/posts', getPosts)
+router.get('/:carrera/posts/:opinion_id', getPost)
 
 export default router;
